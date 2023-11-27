ba4fe6b58c49ef1bc32c37e7e95288be37468f1f
onyekaonwuama <nnyeka212@gmail.com>
2023-11-27 11:27:22 -0800

    database connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 import  mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const uri = process.env.REACT_APP_MONGODB_URI
 
 //the app an instance of express
 const app = express()
@@ -10,6 +16,13 @@ app.use(express.json())
 app.use(cors())
 
 //connect to mongo database
-mongoose.connect();
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
 
 app.listen(3001, () => console.log("server has started"))
